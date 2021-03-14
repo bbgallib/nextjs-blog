0a2861ab6ae45db91270e75637aebe8bfc0ad7a1
bbgallib <bbgallib@gmail.com>
2021-03-15 00:32:45 +0900

    TypeScript導入

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,5 +1,7 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
 // req = HTTP incoming message, res = HTTP server response
 // Learn more about the request handler above in https://nextjs.org/docs/api-routes/introduction
-export default function handler(req, res) {
+export default (_: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ text: 'Hello' })
 }
